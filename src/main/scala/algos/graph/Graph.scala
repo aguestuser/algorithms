457d8321a5e2df75ab5adb5750f2c44f16f53e12
aguestuser <guest.austin@gmail.com>
2015-03-07 15:39:39 -0500

    Refactor graph contraction

--- src/main/scala/algos/graph/Graph.scala
+++ src/main/scala/algos/graph/Graph.scala
@@ -1,5 +1,7 @@
 package algos.graph
 
+import algos.graph.Vertex._
+
 import scala.annotation.tailrec
 import scala.math.random
 
@@ -23,44 +25,42 @@ object Graph {
     val es = v.adj.zipWithIndex map { case(a,i) => Edge(a,v.a) }
     Graph[A](g.vs :+ v, g.es ++ es) }
 
-  def connect[A](g: Graph[A], a: A, b: A): Graph[A] =
-    //TODO use vector indices instead of A values for lookup
-    (findv(g,a),findv(g,b)) match {
-      case (None,_) => throw new Exception(s"Couldn't find node containing $a")
-      case (_,None) => throw new Exception(s"Couldn't find node containing $b")
-      case (Some(u),Some(v)) =>
-        val (uu,vv) = (Vertex(u.a, v.a :: u.adj), Vertex(v.a, u.a :: v.adj))
-        val (ui, vi) = (g.vs.indexOf(u), g.vs.indexOf(v))
-        val ee = Edge(uu.a, vv.a)
-        Graph[A](g.vs.updated(ui, uu).updated(vi, vv), g.es :+ ee) }
+  def connect[A](g: Graph[A], i: Int, j: Int): Graph[A] = {
+    val (vi, vj) = (g.vs(i),g.vs(j))
+    val (vvi,vvj) = (Vertex(vi.a, vj.a :: vi.adj), Vertex(vj.a, vi.a :: vj.adj))
+    Graph[A](g.vs.updated(i, vvi).updated(j, vvj), g.es :+ Edge(vvi.a, vvj.a)) }
+  
+  def connectByKey[A](g: Graph[A], a: A, b: A): Graph[A] =
+    (findV(g,a), findV(g,b)) match {
+      case (None,_) => g
+      case (_,None) => g
+      case (Some(u),Some(v)) => connect(g,g.vs.indexOf(u),g.vs.indexOf(v)) }
 
   @tailrec
   def randomContract[A](g: Graph[A])(implicit o: Ordering[A]): Graph[A] = {
     val gg = contract(g, (random * g.vs.size).toInt, (random * g.vs.size).toInt)
     if (gg.vs.size <= 2) gg else randomContract(gg) }
-
-  def contract[A](g: Graph[A],i: Int, j: Int)(implicit o: Ordering[A]) = {
-    val (vi,vj) = (g.vs(i),g.vs(j))
-    val vvAdj = Vertex.replace(vi.adj ::: vj.adj,vj.a,vi.a) filter { _ != vi.a }
-    val vv = Vertex(vi.a, vvAdj)
-    val vs = g.vs.updated(i, vv).filter(_.a != vj.a)
-    Graph[A](vs,Edge.edges(vs)) }
-
+  def contract[A](g: Graph[A],i: Int, j: Int)(implicit o: Ordering[A]) =
+    Graph[A](contractVs(g.vs,i,j),contractEs(g.es,g.vs,i,j))
+  def contractVs[A](vs: Vector[Vertex[A]], i: Int, j: Int): Vector[Vertex[A]] =
+    vs updated(i,combine(vs(i),vs(j))) map { Vertex.replace(_, vs(j).a, vs(i).a) } filter { _.a != vs(j).a }
+  def contractEs[A](es: Vector[Edge[A]], vs: Vector[Vertex[A]], i: Int, j: Int): Vector[Edge[A]] =
+    es map { Edge.replace(_,vs(j).a,vs(i).a) } filter { { case Edge(a,b) => a != b } }
+  
+  
   def sizes[A](g: Graph[A]): (Int,Int) = (g.vs.size, g.es.size)
   def size[A](g: Graph[A]): Int = sizes(g) match { case(m,n) => m+n }
 
-  def findv[A](g: Graph[A], a: A): Option[Vertex[A]] = g.vs find { _.a == a }
-  def hasv[A](g: Graph[A], a: A): Boolean = g.vs exists { _.a == a }
-  def hase[A](g: Graph[A], e: Edge[A]): Boolean = g.es contains { e }
+  def findV[A](g: Graph[A], a: A): Option[Vertex[A]] = g.vs find { _.a == a }
+  def hasE[A](g: Graph[A], e: Edge[A]): Boolean = g.es contains { e }
 
 }
 
 object Vertex {
-
   def deg[A](v: Vertex[A]): Int = v.adj.size
   def connected[A](u: Vertex[A], v: Vertex[A]): Boolean = u.adj.contains(v)
-  def replace[A](as: List[A], a: A, b: A): List[A] = as.map(x => if (x == a) b else x)
-
+  def combine[A](u: Vertex[A], v: Vertex[A]): Vertex[A] = Vertex(u.a, u.adj.filter(_ != v.a) ::: v.adj.filter(_ != u.a))
+  def replace[A](v: Vertex[A], a: A, b: A): Vertex[A] = Vertex(v.a, v.adj.map(x => if (x == a) b else x))
 }
 
 object Edge {
@@ -71,6 +71,11 @@ object Edge {
 
   def order[A](as:(A,A))(implicit o: Ordering[A]):(A,A) = as match {
     case(a,aa) => if (o.lteq(a,aa)) (a,aa) else (aa,a) }
+
+  def replace[A](e: Edge[A], a: A, b: A): Edge[A] = {
+    def replace1(x: A): A = if (x == a) b else x
+    e match { case Edge(u,v) => Edge(replace1(u),replace1(v)) }
+  }
 
 //  def equiv[A](e1: Edge[A], e2: Edge[A]): Boolean = (e1,e2) match {
 //    case (Edge(u1,v1),(u2,v2)) => (u1 == u2 && v1 == v2) || (u1 == v2 && v1 == u2) }
--- src/test/scala/algos/graph/Graph$Test.scala
+++ src/test/scala/algos/graph/Graph$Test.scala
@@ -28,18 +28,13 @@ class Graph$Test extends Specification {
     }
 
     "find a vertex if it exists" in {
-      findv(g,2) === Some(Vertex(2,List(1,3,4)))
-      findv(g,5) === None
-    }
-
-    "detect if a vertex exists" in {
-      hasv(g,1) === true
-      hasv(g,5) === false
+      findV(g,2) === Some(Vertex(2,List(1,3,4)))
+      findV(g,5) === None
     }
 
     "detect if an edge exists" in {
-      hase(g,Edge(1,2)) === true
-      hase(g,Edge(100,101)) === false
+      hasE(g,Edge(1,2)) === true
+      hasE(g,Edge(100,101)) === false
     }
 
     "add a vertex" in {
@@ -50,24 +45,45 @@ class Graph$Test extends Specification {
       sizes(g2) === (5,6)
     }
 
-    "connect two vertices" in {
+    "connect two vertices by index" in {
+
       val g2 = add(g,Vertex(5,List()))
-      val g3 = connect(g2,1,5)
+      val g3 = connect(g2,0,4)
       g3 === Graph(
         Vector(Vertex(1,List(5,2,3,4)),Vertex(2,List(1,3,4)),Vertex(3,List(1,2,4)),Vertex(4,List(1,2,3)),Vertex(5,List(1))),
         Vector(Edge(1,2),Edge(1,3),Edge(1,4),Edge(2,3),Edge(2,4),Edge(3,4),Edge(1,5)))
       sizes(g3) === (5,7)
     }
 
+    "connect two vertices by key" in {
+      val g2 = add(g,Vertex(5,List()))
+
+      connectByKey(g2,1,1000) === g2
+      connectByKey(g2,1000,1) === g2
+      connectByKey(g2,1000,1000) === g2
+      connectByKey(g2,1,5) === Graph(
+        Vector(Vertex(1,List(5,2,3,4)),Vertex(2,List(1,3,4)),Vertex(3,List(1,2,4)),Vertex(4,List(1,2,3)),Vertex(5,List(1))),
+        Vector(Edge(1,2),Edge(1,3),Edge(1,4),Edge(2,3),Edge(2,4),Edge(3,4),Edge(1,5)))
+    }
+
+    "contract a vector of vertexes" in {
+      contractVs(g.vs,0,1) === Vector(Vertex(1,List(3,4,3,4)), Vertex(3,List(1,1,4)), Vertex(4,List(1,1,3)))
+    }
+
+    "contract a vector of edges" in {
+      contractEs(g.es,g.vs,0,1) === Vector(Edge(1,3),Edge(1,4),Edge(1,3),Edge(1,4),Edge(3,4))
+    }
+
     "contract a graph" in {
       val g2 = contract(g,0,1)
       g2 === Graph(
-//        Vector(Vertex(1,List(2,3,4)),Vertex(2,List(1,3,4)),Vertex(3,List(1,2,4)),Vertex(4,List(1,2,3))),
-//        Vector(Edge(1,2),Edge(1,3),Edge(1,4),Edge(2,3),Edge(2,4),Edge(3,4)))
-        Vector(Vertex(1,List(3,4,3,4)),Vertex(3,List(1,2,4)),Vertex(4,List(1,2,3))),
-        // actual: Vector(Edge(1,3), Edge(1,4), Edge(2,3), Edge(3,4), Edge(2,4)))
-        Vector(Edge(1,3),Edge(1,3),Edge(1,4),Edge(1,4),Edge(2,3),Edge(2,4),Edge(3,4)))
-      sizes(g2) === (3,7)
+        Vector(Vertex(1,List(3,4,3,4)), Vertex(3,List(1,1,4)), Vertex(4,List(1,1,3))),
+        Vector(Edge(1,3),Edge(1,4),Edge(1,3),Edge(1,4),Edge(3,4)))
+      sizes(g2) === (3,5)
+    }
+
+    "recursively and randomly contract a graph" in {
+      sizes(randomContract(g))._1 === 2
     }
   }
 
